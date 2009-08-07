9219b260e100a2f3abbbbf02591ea15ad000804b
Andreas Fidjeland <akf@doc.ic.ac.uk>
2009-08-07 13:40:00 +0100

    Updated Matlab documentation

--- src/client-api/matlab/nemoEnableSTDP.m
+++ src/client-api/matlab/nemoEnableSTDP.m
@@ -2,9 +2,9 @@
 %
 %   nemoEnableSTDP(PRE_FIRE, POST_FIRE, MAX_WEIGHT)
 %
-% If set before nemoStart is called, the backend is configured to run with STDP.
-% It will gather synapses modification statistics continously. To update the
-% synapses using the accumulated values, call nemoApplySTDP.
+% If set before nemoStart is called, the backend is configured to run with
+% STDP.  It will gather synapses modification statistics continously. To update
+% the synapses using the accumulated values, call nemoApplySTDP.
 %
 % Synapses are modified either when a spike arrives either shortly before or
 % shortly after the postsynaptic neuron fires.
@@ -17,11 +17,17 @@
 % the term to add to a synapse for which a spike arrived one cycle before the
 % postsynaptic fired.
 %
-% The length of each array specify the time window during which STDP has an
+% The length of each vector specify the time window during which STDP has an
 % effect.
 %
 % In the regular asymetric STDP, PRE_FIRE leads to potentiation and is hence
 % positive, whereas POST_FIRE leads to depression and is hence negative.
+% However, the STDP function can be configured differently. Any cycle for which
+% the function has a positive value is a potentation cycle, and conversely,
+% negative values indicate depression. This results in two distinct regions of
+% the STDP window. These regions are treated differently. For any postsynaptic
+% firing only the spike whose arrival is closest in time to the firing is used
+% to update the potentation/depression statistics.
 %
 % Only excitatory synapses are affectd by STDP. The weights of the affected
 % synapses are never allowed to increase above MAX_WEIGHT, and likewise not
