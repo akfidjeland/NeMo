1098f113d7fcb32b431170146e9024d23f91dda2
Andreas Fidjeland <akf@doc.ic.ac.uk>
2009-10-22 15:55:13 +0100

    Performance improvements for CPU backend
    - changed neuron array to be immutable
    - add explicit loop for neuron state update
    - removed fired array, instead generate sparse list of firing directly

--- src/Simulation/CPU.hs
+++ src/Simulation/CPU.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Simulation.CPU (initSim) where
 
 import Control.Monad (zipWithM_)
 import Data.Array.IO
+import Data.Array.IArray
 import Data.Array.Base
 import System.Random (StdGen)
 
@@ -24,11 +25,10 @@ import qualified Util.Assocs as A (mapElems)
  - for execution. When executing we need 1) fast random access 2) in-place
  - modification, hence IOArray. -}
 data CpuSimulation = CpuSimulation {
-        network  :: IOArray Idx IzhNeuron,
+        network  :: Array Idx IzhNeuron,
         synapses :: SynapsesRT,
         spikes   :: SpikeQueue,
         currentAcc :: IOUArray Idx FT,
-        fired :: IOUArray Idx Bool,
         currentU :: IOUArray Idx FT,
         currentV :: IOUArray Idx FT,
         -- TODO: also wrap whole array in maybe so we can bypass one pass over array
@@ -57,34 +57,38 @@ initSim net = mkRuntime net
 {- | Perform a single simulation step. Update the state of every neuron and
  - propagate spikes -}
 stepSim :: CpuSimulation -> [Idx] -> IO FiringOutput
-stepSim (CpuSimulation ns ss sq iacc facc uacc vacc rngacc) forcedFiring = do
-    bounds <- getBounds ns
-    let idx = [fst bounds..snd bounds]
+stepSim (CpuSimulation ns ss sq iacc uacc vacc rngacc) forcedFiring = do
+    let bs = bounds ns
+    let idx = [fst bs..snd bs]
     initI <- updateArray thalamicInput rngacc
     zipWithM_ (writeArray iacc) [0..] initI
     accCurrent iacc =<< deqSpikes sq
-    ivals <- getElems iacc -- list of current for each neuron
+    -- TODO: avoid need to densify
     let forced = densify forcedFiring idx
-    zipWithM_ (update ns iacc facc) forced [0..]
-    -- TODO: perhaps generate firing array on the fly
-    assoc' <- getAssocs facc
-    let fired = map fst $ filter snd assoc'
+    fired <- update forced ns iacc uacc vacc
     enqSpikes sq fired ss
     return $! FiringOutput fired
+
+
+update fs !ns !is !us !vs = go fs 0 ns is us vs
     where
-        update ns iacc facc forced idx = do
-            inew <- readArray iacc idx -- accumulated current
-            n <- readArray ns idx
-            -- should perhaps store persistent dynamic state together
-            u <- readArray uacc idx
-            v <- readArray vacc idx
+        go [] _ _ _ _ _ = return []
+        go (forced:fs) !idx !ns !is !us !vs = do
+            -- TODO: check array indices, at least once?
+            let n = ns!idx
+            i <- unsafeRead is idx -- accumulated current
+            -- should perhaps store persistent dynamic state together, to save on lookups
+            u <- unsafeRead us idx
+            v <- unsafeRead vs idx
             let state = IzhState u v
             -- TODO: change argument order
-            let (state', fired) = updateIzh forced inew state n
-            -- TODO: use unsafe writes?
-            writeArray uacc idx $ stateU state'
-            writeArray vacc idx $ stateV state'
-            writeArray facc idx fired
+            let (state', fired) = updateIzh forced i state n
+            unsafeWrite us idx $! stateU state'
+            unsafeWrite vs idx $! stateV state'
+            fs <- go fs (idx+1) ns is us vs
+            if fired
+                then return $! idx : fs
+                else return $!       fs
 
 
 {- | Accumulate current for each neuron for spikes due to be delivered right
@@ -119,23 +123,22 @@ updateArray f xs = getAssocs xs >>= mapM (modify xs f)
 -- pre: neurons in ns are numbered consecutively from 0-size ns-1.
 mkRuntimeN ns =
     if validIdx ns
-        then newListArray (0, Neurons.size ns - 1) (map ndata (Neurons.neurons ns))
+        then listArray (0, Neurons.size ns - 1) (map ndata (Neurons.neurons ns))
         else error "mkRuntimeN: Incorrect indices in neuron map"
     where
         validIdx ns = all (uncurry (==)) (zip [0..] (Neurons.indices ns))
 
 
 mkRuntime net@(Network ns _) = do
-    ns' <- mkRuntimeN ns
+    let ns' = mkRuntimeN ns
     let ss = mkSynapsesRT net
     sq <- mkSpikeQueue net
     -- TODO: do the same bounds checking as for mkRuntimeN
     iacc <- newListArray (0, Neurons.size ns-1) (repeat 0)
-    facc <- newListArray (0, Neurons.size ns-1) (repeat False)
     uacc <- newListArray (0, Neurons.size ns-1) $ map (initU . ndata) $ neurons net
     vacc <- newListArray (0, Neurons.size ns-1) $ map (initV . ndata) $ neurons net
     rngacc <- newListArray (0, Neurons.size ns-1) $ map (stateThalamic . ndata) $ neurons net
-    return $! CpuSimulation ns' ss sq iacc facc uacc vacc rngacc
+    return $! CpuSimulation ns' ss sq iacc uacc vacc rngacc
 
 
 
