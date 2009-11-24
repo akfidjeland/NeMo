595dd1f6eba2e7362e72963ebcc159c50d6389e8
Andreas Fidjeland <akf@doc.ic.ac.uk>
2009-11-24 17:37:46 +0000

    Dead code removal
    Test code for old binary format

--- testsuite/Test/Simulation/Run.hs
+++ testsuite/Test/Simulation/Run.hs
@@ -1,64 +1,27 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 module Test.Simulation.Run (tests) where
 
-import Control.Exception (handle, IOException)
-import System.Directory (removeFile)
-import System.IO (hClose)
-import System.Random (mkStdGen)
 import Test.HUnit
-import Test.QuickCheck (generate)
 
 import Construction.Construction
-import Construction.Izhikevich
-import Construction.Network
-import Construction.Synapse
 import Examples.Smallworld (smallworldOrig)
 import Options (defaults)
 import Simulation.CUDA.Options (cudaOptions)
-import Simulation.FiringStimulus
--- import Simulation.FileSerialisation (encodeSimFile, decodeSimFile)
+import Simulation.FiringStimulus (FiringStimulus(FiringList))
 import Simulation.Options (simOptions, BackendOptions(..), optDuration)
 import Simulation.Run
 import Simulation.STDP.Options (stdpOptions)
 import Types
 
-import Test.Files
 import Test.Comparative (comparisonTest)
 
 tests = TestList [
-        -- TestCase test_fileSerialisation,
-        -- TestCase test_invalidDataDecoding,
         test_repeatedRun
     ]
 
 
--- | Network should be the same after a roundtrip to a file
-{-
-test_fileSerialisation :: Assertion
-test_fileSerialisation = do
-    (f,h) <- openTemp "test_fileSerialisation"
-    hClose h
-    let net1 = generate (1000*20) (mkStdGen 0) (smallworldOrig) :: Network (IzhNeuron FT) Static
-    encodeSimFile f net1 NoFiring
-    (net2, _) <- decodeSimFile f
-    removeFile f
-    assertEqual "file serialisation/deserialisation" net1 net2
--}
 
--- | We should get an exception (rather than stack overflow) if provided with
--- garbage data
-{-
-test_invalidDataDecoding :: Assertion
-test_invalidDataDecoding = do
-    handle (\(_::IOException) -> return ()) $ do
-    (net, _) <- decodeSimFile "/dev/random"
-        :: IO (Network (IzhNeuron FT) Static, FiringStimulus)
-    assertFailure "succeeded in decoding random data!?"
-    -- would be surprising indeed. In practice we'd get a stack overflow
--}
-
--- | We should get the same firing trace when running the same network twice.
--- This tests for the case where residual data from one run affects the next
+{- | We should get the same firing trace when running the same network twice.
+ - This tests for the case where residual data from one run affects the next -}
 test_repeatedRun :: Test
 test_repeatedRun = comparisonTest sim sim "Two subsequent identical simulations"
 
