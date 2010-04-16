9f0deaf165edb468e0e10666f719f76acd1cf14d
Andreas Fidjeland <akf@doc.ic.ac.uk>
2010-04-16 16:17:00 +0100

    Cuda: Fixed whitespace in ThalamicInput class

--- libnemo/src/ThalamicInput.cpp
+++ libnemo/src/ThalamicInput.cpp
@@ -19,19 +19,19 @@ namespace nemo {
 	namespace cuda {
 
 ThalamicInput::ThalamicInput(
-        size_t partitionCount,
-        size_t partitionSize,
-        int seed) :
-    /*! \note For the RNG state we only use one state variable per thread (per
-     * plane), rather than per neuron. We could thus make m_rngState smaller,
-     * but make it the same size so that we can use the same pitch as the other
-     * data structures. */
-    m_rngState(partitionCount, partitionSize, true, 4),
-    m_sigma(partitionCount, partitionSize, true, 1),
-    m_inUse(false),
-    m_partitionCount(partitionCount),
-    m_partitionSize(partitionSize),
-    m_seed(seed)
+		size_t partitionCount,
+		size_t partitionSize,
+		int seed) :
+	/*! \note For the RNG state we only use one state variable per thread (per
+	 * plane), rather than per neuron. We could thus make m_rngState smaller,
+	 * but make it the same size so that we can use the same pitch as the other
+	 * data structures. */
+	m_rngState(partitionCount, partitionSize, true, 4),
+	m_sigma(partitionCount, partitionSize, true, 1),
+	m_inUse(false),
+	m_partitionCount(partitionCount),
+	m_partitionSize(partitionSize),
+	m_seed(seed)
 { }
 
 
@@ -56,11 +56,11 @@ ThalamicInput::d_allocated() const
 void
 ThalamicInput::moveToDevice()
 {
-    if(m_inUse) {
-        initRngState();
-        m_rngState.moveToDevice();
-        m_sigma.moveToDevice();
-    }
+	if(m_inUse) {
+		initRngState();
+		m_rngState.moveToDevice();
+		m_sigma.moveToDevice();
+	}
 }
 
 
@@ -76,18 +76,18 @@ ThalamicInput::initRngState()
 	boost::variate_generator<rng_t, boost::uniform_int<unsigned long> >
 		seed(rng, boost::uniform_int<unsigned long>(0, 0x7fffffff));
 
-    /* This RNG state vector needs to be filled with initialisation data.  Each
-     * RNG needs 4 32-bit words of seed data, with each thread having a
-     * diferent seed. */
-    std::vector<unsigned> rngbuf(m_partitionSize);
-    for(unsigned partition=0; partition<m_partitionCount; ++partition) {
-        for(unsigned plane=0; plane<4; ++plane) {
-            for(unsigned i=0; i<rngbuf.size(); ++i) {
+	/* This RNG state vector needs to be filled with initialisation data.  Each
+	 * RNG needs 4 32-bit words of seed data, with each thread having a
+	 * diferent seed. */
+	std::vector<unsigned> rngbuf(m_partitionSize);
+	for(unsigned partition=0; partition<m_partitionCount; ++partition) {
+		for(unsigned plane=0; plane<4; ++plane) {
+			for(unsigned i=0; i<rngbuf.size(); ++i) {
 				rngbuf[i] = seed();
-            }
-            m_rngState.setPartition(partition, &rngbuf[0], rngbuf.size(), plane);
-        }
-    }
+			}
+			m_rngState.setPartition(partition, &rngbuf[0], rngbuf.size(), plane);
+		}
+	}
 }
 
 
@@ -95,25 +95,26 @@ ThalamicInput::initRngState()
 unsigned* 
 ThalamicInput::deviceRngState() const
 {
-    return m_inUse ? m_rngState.deviceData() : NULL;
+	return m_inUse ? m_rngState.deviceData() : NULL;
 }
+
 
 
 float* 
 ThalamicInput::deviceSigma() const
 {
-    return m_inUse ? m_sigma.deviceData() : NULL;
+	return m_inUse ? m_sigma.deviceData() : NULL;
 }
 
 
 size_t
 ThalamicInput::wordPitch() const
 {
-    size_t p1 = m_rngState.wordPitch();
-    size_t p2 = m_sigma.wordPitch();
-    //! \todo throw exception here instead
-    assert(p1 == p2);
-    return p1 == p2 ? p1 : 0;
+	size_t p1 = m_rngState.wordPitch();
+	size_t p2 = m_sigma.wordPitch();
+	//! \todo throw exception here instead
+	assert(p1 == p2);
+	return p1 == p2 ? p1 : 0;
 }
 
 	} // end namespace cuda
