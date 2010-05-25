6360ff16b722e7c15750a4af3f16beef4d3c6c39
Andreas Fidjeland <akf@doc.ic.ac.uk>
2010-05-25 15:46:13 +0100

    MPI: added serialisation of neurons and synapses
    Extended existing types. Added a new synapse type.

--- libnemo/src/types.hpp
+++ libnemo/src/types.hpp
@@ -10,36 +10,113 @@
  * licence along with nemo. If not, see <http://www.gnu.org/licenses/>.
  */
 
+
 /* The basic types in nemo_types are also used without an enclosing namespace
  * inside the kernel (which is pure C). */
+#include <nemo_config.h>
 #include "types.h"
 
+#ifdef INCLUDE_MPI
+#include <boost/serialization/serialization.hpp>
+#endif
+
+
+#ifdef INCLUDE_MPI
+namespace boost {
+	namespace serialization {
+		class access;
+	}
+}
+#endif
 
 namespace nemo {
 
 
 template<typename FP>
-struct Neuron {
+class Neuron
+{
+	public :
 
-	Neuron(): a(0), b(0), c(0), d(0), u(0), v(0), sigma(0) {}
+		Neuron(): a(0), b(0), c(0), d(0), u(0), v(0), sigma(0) {}
 
-	Neuron(FP a, FP b, FP c, FP d, FP u, FP v, FP sigma) :
-		a(a), b(b), c(c), d(d), u(u), v(v), sigma(sigma) {}
+		Neuron(FP a, FP b, FP c, FP d, FP u, FP v, FP sigma) :
+			a(a), b(b), c(c), d(d), u(u), v(v), sigma(sigma) {}
 
-	FP a, b, c, d, u, v, sigma;
+		FP a, b, c, d, u, v, sigma;
+
+	private :
+
+#ifdef INCLUDE_MPI
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive & ar, const unsigned int version) {
+			ar & a;
+			ar & b;
+			ar & c;
+			ar & d;
+			ar & u;
+			ar & v;
+			ar & sigma;
+		}
+#endif
+
 };
 
 
 
-template<typename I, typename W>
-struct Synapse
-{
-	I target;
-	W weight;
-	//! \todo change to bool?
-	unsigned char plastic;
 
-	Synapse(I t, W w, unsigned char p) : target(t), weight(w), plastic(p) {}
+template<typename I, typename W>
+class Synapse
+{
+	public :
+
+		I target;
+		W weight;
+		//! \todo change to bool?
+		unsigned char plastic;
+
+		Synapse() : target(0), weight(0.0f), plastic(false) { }
+
+		Synapse(I t, W w, unsigned char p) : target(t), weight(w), plastic(p) {}
+
+	private :
+#ifdef INCLUDE_MPI
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive & ar, const unsigned int version) {
+			ar & target;
+			ar & weight;
+			ar & plastic;
+		}
+#endif
+};
+
+
+
+//! \todo change names to Synapse and AxonTerminal
+template<typename I, typename D, typename W>
+class FullSynapse
+{
+	public :
+
+		I source;
+		D delay;
+		Synapse<I, W> terminal;
+
+	private :
+#ifdef INCLUDE_MPI
+		friend class boost::serialization::access;
+
+		template<class Archive>
+		void serialize(Archive & ar, const unsigned int version) {
+			ar & source;
+			ar & delay;
+			ar & terminal;
+		}
+
+#endif
 };
 
 
