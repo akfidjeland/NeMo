b32705f2a3c38628f3d1072942ccd84c814d008d
Andreas Fidjeland <akf@doc.ic.ac.uk>
2010-05-25 12:32:50 +0100

    Added use of shared_ptr in FiringOutput class

--- libnemo/src/FiringOutput.cpp
+++ libnemo/src/FiringOutput.cpp
@@ -23,8 +23,6 @@ FiringOutput::FiringOutput(
 		size_t partitionCount,
 		size_t partitionSize,
 		unsigned maxReadPeriod):
-	md_buffer(NULL),
-	mh_buffer(NULL),
 	m_pitch(0),
 	m_partitionCount(partitionCount),
 	m_bufferedCycles(0),
@@ -36,22 +34,17 @@ FiringOutput::FiringOutput(
 	size_t height = partitionCount * maxReadPeriod;
 
 	size_t bytePitch;
-	CUDA_SAFE_CALL(cudaMallocPitch((void**)(&md_buffer), &bytePitch, width, height));
-	CUDA_SAFE_CALL(cudaMemset2D(md_buffer, bytePitch, 0, bytePitch, height));
+	uint32_t* d_buffer;
+	CUDA_SAFE_CALL(cudaMallocPitch((void**)(&d_buffer), &bytePitch, width, height));
+	md_buffer = boost::shared_ptr<uint32_t>(d_buffer, cudaFree);
+	CUDA_SAFE_CALL(cudaMemset2D(d_buffer, bytePitch, 0, bytePitch, height));
 	m_pitch = bytePitch / sizeof(uint32_t);
 
 	size_t md_allocated = bytePitch * height;
-	CUDA_SAFE_CALL(cudaMallocHost((void**) &mh_buffer, md_allocated));
-	memset(mh_buffer, 0, md_allocated);
-}
-
-
-
-FiringOutput::~FiringOutput()
-{
-	//! \todo use shared_ptr here to do the freeing
-	cudaFreeHost(mh_buffer);
-	cudaFree(md_buffer);
+	uint32_t* h_buffer;
+	CUDA_SAFE_CALL(cudaMallocHost((void**) &h_buffer, md_allocated));
+	mh_buffer = boost::shared_ptr<uint32_t>(h_buffer, cudaFreeHost);
+	memset(h_buffer, 0, md_allocated);
 }
 
 
@@ -59,11 +52,11 @@ FiringOutput::~FiringOutput()
 uint32_t*
 FiringOutput::step()
 {
-	uint32_t* ret = md_buffer + m_bufferedCycles * m_partitionCount * m_pitch;
+	uint32_t* ret = md_buffer.get() + m_bufferedCycles * m_partitionCount * m_pitch;
 	m_bufferedCycles += 1;
 	if(m_bufferedCycles > m_maxReadPeriod) {
 		m_bufferedCycles = 0;
-		ret = md_buffer;
+		ret = md_buffer.get();
 		throw std::runtime_error("Firing buffer overflow");
 	}
 	return ret;
@@ -79,12 +72,11 @@ FiringOutput::readFiring(
 	m_cycles.clear();
 	m_neuronIdx.clear();
 
-	//! \todo error handling
-	CUDA_SAFE_CALL(cudaMemcpy(mh_buffer,
-				md_buffer,
+	CUDA_SAFE_CALL(cudaMemcpy(mh_buffer.get(),
+				md_buffer.get(),
 				m_bufferedCycles * m_partitionCount * m_pitch * sizeof(uint32_t),
 				cudaMemcpyDeviceToHost));
-	populateSparse(m_bufferedCycles, mh_buffer, m_cycles, m_neuronIdx);
+	populateSparse(m_bufferedCycles, mh_buffer.get(), m_cycles, m_neuronIdx);
 
 	*cycles = &m_cycles;
 	*neuronIdx = &m_neuronIdx;
