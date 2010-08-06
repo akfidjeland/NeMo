38f0c5d45106647ddf2978552ab9e820d3ed0781
Andreas Fidjeland <akf@doc.ic.ac.uk>
2010-08-06 12:24:51 +0100

    Bugfix: mismatch between local and driver device ids
    On systems with several devices, only some of which are usable by libnemo, we'd
    get a failure due to mixing up these two different device numbering schemes.

--- libnemo/nemo/cuda/devices.cpp
+++ libnemo/nemo/cuda/devices.cpp
@@ -2,6 +2,7 @@
 
 
 #include <map>
+#include <vector>
 #include <boost/format.hpp>
 
 #include <nemo/config.h>
@@ -14,18 +15,101 @@
 namespace nemo {
 	namespace cuda {
 
+/* We want to present users of libnemo with a contiguous range of device indices.
+ * Since the host system may have devices which are not suitable for use by
+ * nemo, the device indexing may differ between nemo and the CUDA driver. We
+ * refer to our own ids as *local* ids, and the CUDA ids as *driver ids.
+ */
 
-typedef std::map<unsigned, cudaDeviceProp> devmap_t;
 
-devmap_t* g_devices = NULL;
+class DeviceMap
+{
+	public :
 
-unsigned g_defaultDevice = -1;
+		static DeviceMap* instance();
+
+		typedef unsigned local_id;
+		typedef int driver_id;
+
+		unsigned deviceCount() const { return m_devices.size(); }
+
+		/* \post the return value is a valid index in the device list */
+		driver_id driverId(local_id) const;
+
+		void chooseDevice(ConfigurationImpl& conf, int device) const;
+
+		const char* description(local_id device) const;
+
+	private :
+
+		static DeviceMap* s_instance;
+
+		DeviceMap();
+
+		/* All the suitable devices, indexed by driver id */
+		std::map<driver_id, cudaDeviceProp> m_devices;
+
+		/* Mapping from local ids to driver ids */
+		std::vector<driver_id> m_driverIds;
+
+		/* Default device to use (local id) */
+		local_id m_defaultDevice;
+
+		bool deviceSuitable(driver_id device);
+
+		void setDefaultDevice(const std::vector<int>& driverIds);
+
+		bool validLocalId(local_id id) const { return id < m_driverIds.size(); }
+};
 
 
-#ifndef __DEVICE_EMULATION__
+DeviceMap* DeviceMap::s_instance = NULL;
+
+
+DeviceMap*
+DeviceMap::instance()
+{
+	if(s_instance == NULL) {
+		s_instance = new DeviceMap();
+	}
+	return s_instance;
+}
+
+
+
+
+DeviceMap::DeviceMap() :
+	m_defaultDevice(-1)
+{
+#ifdef __DEVICE_EMULATION__
+	cudaDeviceProp prop;
+	prop.major = 9999;
+	prop.minor = 9999;
+	driver_id dev;
+	CUDA_SAFE_CALL(cudaChooseDevice(&dev, &prop));
+	m_devices[0] = prop;
+	m_defaultDevice = 0;
+	m_driverIds.push_back(0);
+#else
+	driver_id dcount = 0;
+	CUDA_SAFE_CALL(cudaGetDeviceCount(&dcount));
+	for(driver_id device = 0; device < dcount; ++device) {
+		if(deviceSuitable(device)) {
+			cudaDeviceProp prop;
+			CUDA_SAFE_CALL(cudaGetDeviceProperties(&prop, device));
+			m_devices[device] = prop;
+			m_driverIds.push_back(device);
+		}
+	}
+
+	setDefaultDevice(m_driverIds);
+#endif
+}
+
+
 
 bool
-deviceSuitable(unsigned device)
+DeviceMap::deviceSuitable(driver_id device)
 {
 	cudaDeviceProp prop;
 	CUDA_SAFE_CALL(cudaGetDeviceProperties(&prop, device));
@@ -47,10 +131,12 @@ deviceSuitable(unsigned device)
 
 
 
-
-unsigned
-getDefaultDevice(const devmap_t& devmap)
+void
+DeviceMap::setDefaultDevice(const std::vector<int>& driverIds)
 {
+	if(driverIds.empty())
+		return;
+
 	cudaDeviceProp prop;
 	prop.major = 1;
 	prop.minor = 2;
@@ -58,109 +144,55 @@ getDefaultDevice(const devmap_t& devmap)
 	int dev;
 	CUDA_SAFE_CALL(cudaChooseDevice(&dev, &prop));
 
-	/* The chose device should already be present in the device map */
-	if(devmap.find(dev) == devmap.end()) {
-		throw nemo::exception(NEMO_LOGIC_ERROR, "Internal error: default device not in device map");
-	}
-
-	return unsigned(dev);
-}
-
-#endif
-
-
-
-/* Create and populate a (global) list of suitable devices */
-const devmap_t&
-enumerateDevices()
-{
-	if(g_devices == NULL) {
-
-		g_devices = new devmap_t();
-#ifdef __DEVICE_EMULATION__
-		cudaDeviceProp prop;
-		prop.major = 9999;
-		prop.minor = 9999;
-		int dev;
-		CUDA_SAFE_CALL(cudaChooseDevice(&dev, &prop));
-		(*g_devices)[0] = prop;
-		g_defaultDevice = 0;
-#else
-		int dcount = 0;
-		CUDA_SAFE_CALL(cudaGetDeviceCount(&dcount));
-		for(int device = 0; device < dcount; ++device) {
-			if(deviceSuitable(device)) {
-				cudaDeviceProp prop;
-				CUDA_SAFE_CALL(cudaGetDeviceProperties(&prop, device));
-				(*g_devices)[device] = prop;
-			}
+	/* The chosen device should already be present in the device map */
+	for(unsigned localId = 0; localId < driverIds.size(); ++localId) {
+		if(driverIds.at(localId) == dev) {
+			m_defaultDevice = localId;
+			return;
 		}
-
-		g_defaultDevice = getDefaultDevice(*g_devices);
-#endif
 	}
-	return *g_devices;
+	throw nemo::exception(NEMO_LOGIC_ERROR, "Internal error: default device not in device map");
 }
 
 
-unsigned
-deviceCount()
+
+DeviceMap::driver_id
+DeviceMap::driverId(local_id l_id) const
 {
-	return enumerateDevices().size();
+	using boost::format;
+
+	if(l_id >= m_driverIds.size()) {
+		throw nemo::exception(NEMO_CUDA_ERROR, str(format("Invalid device: %u") % l_id));
+	}
+
+	driver_id d_id = m_driverIds.at(l_id);
+	if(m_devices.find(d_id) == m_devices.end()) {
+		throw nemo::exception(NEMO_CUDA_ERROR, str(format("Invalid device: %u") % l_id));
+	}
+	return d_id;
 }
 
 
 const char*
-deviceDescription(unsigned device)
+DeviceMap::description(local_id device) const
 {
-	using boost::format;
-
-	const devmap_t& devices = enumerateDevices();
-	devmap_t::const_iterator i = devices.find(device);
-	if(i == devices.end()) {
-		throw nemo::exception(NEMO_CUDA_ERROR, str(format("Invalid device: %u") % device));
-	}
-
-	return i->second.name;
-}
-
-
-
-void
-setDevice(unsigned device)
-{
-	using boost::format;
-
-	const devmap_t& devmap = enumerateDevices();
-	if(devmap.find(device) == devmap.end()) {
-		throw nemo::exception(NEMO_CUDA_ERROR, str(format("Invalid device: %u") % device));
-	}
-
-	int userDev = int(device);
-	int existingDev;
-	CUDA_SAFE_CALL(cudaGetDevice(&existingDev));
-	if(existingDev != userDev) {
-		/* If the device has already been set, we'll get an error here */
-		CUDA_SAFE_CALL(cudaSetDevice(userDev));
-	}
+	return m_devices.find(m_driverIds.at(device))->second.name;
 }
 
 
 void
-chooseDevice(ConfigurationImpl& conf, int device)
+DeviceMap::chooseDevice(ConfigurationImpl& conf, int device) const // local Id
 {
 	using boost::format;
 
-	const devmap_t& devmap = enumerateDevices();
 	if(deviceCount() == 0) {
 		throw nemo::exception(NEMO_CUDA_ERROR, "No CUDA devices available");
 	}
 	conf.setBackend(NEMO_BACKEND_CUDA);
-	if(device < 0) {
-		/* Use default */
-		conf.setCudaDevice(nemo::cuda::g_defaultDevice);
+	if(device < 0) { // Use default
+		conf.setCudaDevice(m_defaultDevice);
 	} else {
-		if(devmap.find(device) == devmap.end()) {
+		if(!validLocalId(device)) {
 			throw nemo::exception(NEMO_CUDA_ERROR, str(format("Invalid device: %u") % device));
 		}
 		conf.setCudaDevice(unsigned(device));
@@ -169,6 +201,20 @@ chooseDevice(ConfigurationImpl& conf, int device)
 	conf.setCudaPartitionSize(MAX_PARTITION_SIZE);
 	conf.setCudaFiringBufferLength(FiringOutput::defaultBufferLength());
 }
+
+
+void
+setDevice(DeviceMap::local_id device)
+{
+	int userDev = DeviceMap::instance()->driverId(device);
+	int existingDev;
+	CUDA_SAFE_CALL(cudaGetDevice(&existingDev));
+	if(existingDev != userDev) {
+		/* If the device has already been set, we'll get an error here */
+		CUDA_SAFE_CALL(cudaSetDevice(userDev));
+	}
+}
+
 
 
 }	}
@@ -180,21 +226,15 @@ chooseDevice(ConfigurationImpl& conf, int device)
 void
 nemo_cuda_choose_device(nemo::ConfigurationImpl* conf, int device)
 {
-	nemo::cuda::chooseDevice(*conf, device);
+	nemo::cuda::DeviceMap::instance()->chooseDevice(*conf, device);
 }
 
-
-void
-nmoe_cuda_test_configuration(nemo::ConfigurationImpl)
-{
-
-}
 
 
 unsigned
 nemo_cuda_device_count()
 {
-	return nemo::cuda::deviceCount();
+	return nemo::cuda::DeviceMap::instance()->deviceCount();
 }
 
 
@@ -202,5 +242,5 @@ nemo_cuda_device_count()
 const char*
 nemo_cuda_device_description(unsigned device)
 {
-	return nemo::cuda::deviceDescription(device);
+	return nemo::cuda::DeviceMap::instance()->description(device);
 }
