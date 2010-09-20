akfidjeland/NeMo

commit 9937770d10352a8cb96c71b83551aed121bf17ee

Cuda: refactoring: moved functionality from Outgoing to WarpAddressTable
- per-neuron warp counting
- total warp counting

libnemo/nemo/cuda/WarpAddressTable.cpp
@@ -17,20 +17,28 @@ namespace nemo {
 	namespace cuda {
 
 
+WarpAddressTable::WarpAddressTable() :
+	m_warpCount(0)
+{
+	;
+}
+
 
 SynapseAddress
 WarpAddressTable::addSynapse(const DeviceIdx& source, pidx_t targetPartition, delay_t delay, size_t nextFreeWarp)
 {
 	idx_t idx(source.partition, source.neuron, targetPartition, delay);
 
-	unsigned& rowLength = m_rowLength[idx];
-	unsigned column = rowLength % WARP_SIZE;
-	rowLength += 1;
+	unsigned& rowSynapses = m_rowSynapses[idx];
+	unsigned column = rowSynapses % WARP_SIZE;
+	rowSynapses += 1;
 
 	std::set<size_t>& warps = m_warps[idx];
 
 	if(column == 0) {
 		warps.insert(nextFreeWarp);
+		m_warpsPerNeuron[source] += 1;
+		m_warpCount += 1;
 		return SynapseAddress(nextFreeWarp, column);
 	} else {
 		return SynapseAddress(*warps.rbegin(), column);
@@ -78,6 +86,14 @@ WarpAddressTable::const_iterator
 WarpAddressTable::warps_end(pidx_t sp, nidx_t sn, pidx_t tp, delay_t d) const
 {
 	return warpSet(sp, sn, tp, d).end();
+}
+
+
+
+unsigned
+WarpAddressTable::maxWarpsPerNeuron() const
+{
+	return std::max_element(m_warpsPerNeuron.begin(), m_warpsPerNeuron.end())->second;
 }
 
 

libnemo/nemo/cuda/ConnectivityMatrix.cpp
@@ -169,8 +169,6 @@ ConnectivityMatrix::createFcm(
 				size_t warps = DIV_CEIL(bundle.size(), WARP_SIZE);
 				size_t words = warps * WARP_SIZE;
 
-				SynapseAddress addr = wtable.addSynapse(d_sourceIdx, d_targetPartition, delay, currentWarp);
-
 				//! \todo allocate these only only once (in outer context)
 				/* Stage new addresses/weights in temporary buffer. We can re-order
 				 * this buffer before writing to h_targets/h_weights in order
@@ -181,7 +179,8 @@ ConnectivityMatrix::createFcm(
 				for(std::vector<AxonTerminal>::const_iterator s = bundle.begin();
 						s != bundle.end(); ++s) {
 
-					// SynapseAddress addr = wtable.addSynapse(d_sourceIdx, d_targetPartition, delay, currentWarp);
+					// SynapseAddress addr =
+					wtable.addSynapse(d_sourceIdx, d_targetPartition, delay, currentWarp);
 					//! \todo update currentWarp and resize host buffer if relevant
 
 					size_t sidx = s - bundle.begin();

libnemo/nemo/cuda/Mapper.hpp
@@ -33,6 +33,15 @@ struct DeviceIdx
 };
 
 
+inline
+bool
+operator<(const DeviceIdx& lhs, const DeviceIdx& rhs)
+{
+	return lhs.partition < rhs.partition ||
+		(lhs.partition == rhs.partition && lhs.neuron < rhs.neuron);
+}
+
+
 
 /*! Maps between device and global indices */
 class Mapper {

libnemo/nemo/cuda/Outgoing.cpp
@@ -93,18 +93,6 @@ Outgoing::reportWarpSizeHistogram(std::ostream& out) const
 
 
 
-size_t
-Outgoing::maxPitch() const
-{
-	size_t pitch = 0;
-	for(map_t::const_iterator i = m_acc.begin(); i != m_acc.end(); ++i) {
-		pitch = std::max(pitch, warpCount(i->second));
-	}
-	return pitch;
-}
-
-
-
 bool
 compare_warp_counts(
 		const std::pair<pidx_t, size_t>& lhs,
@@ -121,7 +109,7 @@ Outgoing::moveToDevice(size_t partitionCount, const WarpAddressTable& wtable)
 	using namespace boost::tuples;
 
 	size_t height = partitionCount * MAX_PARTITION_SIZE;
-	size_t width = maxPitch() * sizeof(outgoing_t);
+	size_t width = wtable.maxWarpsPerNeuron() * sizeof(outgoing_t);
 
 	// allocate device memory for table
 	outgoing_t* d_arr = NULL;

libnemo/nemo/cuda/Outgoing.hpp
@@ -65,7 +65,6 @@ class Outgoing
 
 		map_t m_acc;
 
-		size_t maxPitch() const;
 		size_t warpCount(const targets_t& targets) const;
 
 		size_t m_allocated;

libnemo/nemo/cuda/WarpAddressTable.hpp
@@ -36,6 +36,8 @@ class WarpAddressTable
 
 	public :
 
+		WarpAddressTable();
+
 		/*
 		 * \param nextFreeWarp
 		 * 		The next unused warp in the host FCM.
@@ -54,6 +56,10 @@ class WarpAddressTable
 		const_iterator warps_begin(pidx_t, nidx_t, pidx_t, delay_t) const;
 		const_iterator warps_end(pidx_t, nidx_t, pidx_t, delay_t) const;
 
+		unsigned warpCount() const { return m_warpCount; }
+
+		unsigned maxWarpsPerNeuron() const;
+
 	private :
 
 		typedef std::map<idx_t, warp_set> warp_map;
@@ -63,7 +69,11 @@ class WarpAddressTable
 		const WarpAddressTable::warp_set&
 			warpSet(pidx_t sp, nidx_t sn, pidx_t tp, delay_t d) const;
 
-		std::map<idx_t, unsigned> m_rowLength;
+		std::map<idx_t, unsigned> m_rowSynapses;
+
+		std::map<DeviceIdx, unsigned> m_warpsPerNeuron;
+
+		unsigned m_warpCount;
 };
 
 	} // end namespace cuda

commit ecc498f33c712b864eaf41b05b3beeee904170c8

Bug fix: incorrect max warps per neuron
Bug introduced in
	commit 9937770d10352a8cb96c71b83551aed121bf17ee
	Refs: 0.6.1-23-g9937770
	Author: Andreas Fidjeland <akf@doc.ic.ac.uk>
	Date:   Fri Sep 10 11:30:52 2010 +0100

libnemo/nemo/cuda/WarpAddressTable.cpp
@@ -87,5 +87,23 @@ WarpAddressTable::reportWarpSizeHistogram(std::ostream& out) const
 }
 
 
+bool
+value_compare(const std::pair<DeviceIdx,
+		unsigned>& lhs, const std::pair<DeviceIdx, unsigned>& rhs)
+{
+	return lhs.second < rhs.second;
+}
+
+
+unsigned
+WarpAddressTable::maxWarpsPerNeuron() const
+{
+	if(m_warpsPerNeuron.empty()) {
+		return 0;
+	}
+	return std::max_element(m_warpsPerNeuron.begin(), m_warpsPerNeuron.end(), value_compare)->second;
+}
+
+
 	} // end namespace cuda
 } // end namespace nemo