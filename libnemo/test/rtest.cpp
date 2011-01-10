6682101a510558182a05adc0f75a48416a051fba
Andreas Fidjeland <akf@doc.ic.ac.uk>
2011-01-10 22:16:09 +0000

    Test: added current and firing stimulus to regression tests

--- libnemo/test/rtest.cpp
+++ libnemo/test/rtest.cpp
@@ -5,6 +5,7 @@
 
 #include <boost/test/unit_test.hpp>
 #include <boost/scoped_ptr.hpp>
+#include <boost/random.hpp>
 
 #include <nemo.hpp>
 #include <examples.hpp>
@@ -13,18 +14,30 @@
 #include "rtest.hpp"
 
 
+typedef boost::mt19937 rng_t;
+typedef boost::variate_generator<rng_t&, boost::uniform_int<> > uirng_t;
+
+
 void
 run(nemo::Network* net, 
 	backend_t backend,
 	unsigned seconds,
 	const std::string& filename,
 	bool stdp,
+	bool stimulus, // add both current and firing stimulus
 	bool creating) // are we comparing against existing data or creating fresh data
 {
 	using namespace std;
 
+	rng_t rng;
+	// assume contigous 0-based indices
+	uirng_t randomNeuron(rng, boost::uniform_int<>(0, net->neuronCount()-1));
+
 	nemo::Configuration conf = configuration(stdp, 1024, backend);
-	std::cerr << "running test on " << conf.backendDescription() << " with stdp=" << stdp << std::endl;
+	std::cerr << "running test on " << conf.backendDescription()
+		<< " with stdp=" << stdp
+		<< " and stimulus=" << stimulus << std::endl;
+
 
 	fstream file;
 	//! \todo determine canonical filename based on configuration
@@ -38,9 +51,20 @@ run(nemo::Network* net,
 
 	unsigned ce, ne; // expexted values
 
+	nemo::Simulation::firing_stimulus firingStimulus;
+	nemo::Simulation::current_stimulus currentStimulus;
+
 	for(unsigned s = 0; s < seconds; ++s) {
 		for(unsigned ms = 0; ms < 1000; ++ms) {
-			const std::vector<unsigned>& fired = sim->step();
+
+			if(stimulus) {
+				firingStimulus.resize(1);
+				firingStimulus[0] = randomNeuron();
+				currentStimulus.resize(1);
+				currentStimulus[0] = std::make_pair(randomNeuron(), 0.1f);
+			}
+
+			const std::vector<unsigned>& fired = sim->step(firingStimulus, currentStimulus);
 			for(std::vector<unsigned>::const_iterator ni = fired.begin(); ni != fired.end(); ++ni) {
 				unsigned c = s * 1000 + ms;
 				unsigned n = *ni;
@@ -76,17 +100,21 @@ void runTorus(bool creating)
 		bool stdp = false;
 		boost::scoped_ptr<nemo::Network> torus(nemo::torus::construct(4, 1000, stdp, 64, false));
 #ifdef NEMO_CUDA_ENABLED
-		run(torus.get(), NEMO_BACKEND_CUDA, 4, "test-cuda.dat", stdp, creating);
+		run(torus.get(), NEMO_BACKEND_CUDA, 4, "test-cuda.dat", stdp, false, creating);
+		run(torus.get(), NEMO_BACKEND_CUDA, 4, "test-cuda-stim.dat", stdp, true, creating);
 #endif
-		run(torus.get(), NEMO_BACKEND_CPU, 4, "test-cpu.dat", stdp, creating);
+		run(torus.get(), NEMO_BACKEND_CPU, 4, "test-cpu.dat", stdp, false, creating);
+		run(torus.get(), NEMO_BACKEND_CPU, 4, "test-cpu-stim.dat", stdp, true, creating);
 	}
 
 	{
 		bool stdp = true;
 		boost::scoped_ptr<nemo::Network> torus(nemo::torus::construct(4, 1000, stdp, 64, false));
 #ifdef NEMO_CUDA_ENABLED
-		run(torus.get(), NEMO_BACKEND_CUDA, 4, "test-cuda-stdp.dat", stdp, creating);
+		run(torus.get(), NEMO_BACKEND_CUDA, 4, "test-cuda-stdp.dat", stdp, false, creating);
+		run(torus.get(), NEMO_BACKEND_CUDA, 4, "test-cuda-stdp-stim.dat", stdp, true, creating);
 #endif
-		run(torus.get(), NEMO_BACKEND_CPU, 4, "test-cpu-stdp.dat", stdp, creating);
+		run(torus.get(), NEMO_BACKEND_CPU, 4, "test-cpu-stdp.dat", stdp, false, creating);
+		run(torus.get(), NEMO_BACKEND_CPU, 4, "test-cpu-stdp-stim.dat", stdp, true, creating);
 	}
 }
