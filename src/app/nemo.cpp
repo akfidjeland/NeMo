eb2f2115096046192dfb9ea760424b509358e3eb
Andreas Fidjeland <akf@doc.ic.ac.uk>
2011-03-08 12:52:26 +0000

    Added simple query application

---
+++ src/app/nemo.cpp
@@ -0,0 +1,76 @@
+/* Copyright 2010 Imperial College London
+ *
+ * This file is part of NeMo.
+ *
+ * This software is licenced for non-commercial academic use under the GNU
+ * General Public Licence (GPL). You should have received a copy of this
+ * licence along with nemo. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This is a top-level utility app for NeMo, which currently only prints
+ * information about the library and the parts of the systems on which it can
+ * run.
+ */
+
+#include <iostream>
+#include <boost/program_options.hpp>
+
+#include <nemo.hpp>
+
+
+void
+listCudaDevices()
+{
+	unsigned dcount  = nemo::cudaDeviceCount();
+
+	if(dcount == 0) {
+		std::cout << "No CUDA devices available\n";
+		return;
+	}
+
+	for(unsigned d = 0; d < dcount; ++d) {
+		std::cout << d << ": " << nemo::cudaDeviceDescription(d) << std::endl;
+	}
+}
+
+
+
+int
+main(int argc, char* argv[])
+{
+	namespace po = boost::program_options;
+	po::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "print this message")
+		("version,v", "print version number")
+		("list-devices,l", "print the available simulation devices")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if(vm.count("help") != 0) {
+		std::cout << "Usage:\n\t" << argv[0] << " [OPTIONS]\n\n";
+		std::cout << desc << std::endl;
+		exit(0);
+	}
+
+	if(vm.count("version") != 0) {
+		std::cout << nemo::version() << std::endl;
+		//! \todo list exact build as well
+		//! \todo list build options as well
+		exit(0);
+	}
+
+	if(vm.count("list-devices") != 0) {
+		listCudaDevices();
+		exit(0);
+	}
+
+	/* No options chosen, so return error */
+
+	std::cout << "Usage:\n\t" << argv[0] << " [OPTIONS]\n\n";
+	std::cout << desc << std::endl;
+	return 1;
+}
